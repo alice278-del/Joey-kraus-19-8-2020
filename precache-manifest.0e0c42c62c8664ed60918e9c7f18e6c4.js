c4ed73e6e3af4751293be163724797ea08703d0f
alice278-del <alice.27_8@outlook.com>
2020-08-19 14:02:48 +0300

    Updates

---
+++ precache-manifest.0e0c42c62c8664ed60918e9c7f18e6c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2708a29706d90c384ef24379f5c3a38",
+    "url": "/Joey-Kraus-19-8-2020/index.html"
+  },
+  {
+    "revision": "e5b086f2d5180a00fcb2",
+    "url": "/Joey-Kraus-19-8-2020/static/css/main.b3d7b386.chunk.css"
+  },
+  {
+    "revision": "9fd6ddd754e4d787bd35",
+    "url": "/Joey-Kraus-19-8-2020/static/js/2.0a787867.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/Joey-Kraus-19-8-2020/static/js/2.0a787867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b086f2d5180a00fcb2",
+    "url": "/Joey-Kraus-19-8-2020/static/js/main.1166963f.chunk.js"
+  },
+  {
+    "revision": "8c218ad10732a42275c5",
+    "url": "/Joey-Kraus-19-8-2020/static/js/runtime-main.0534c8a1.js"
+  }
+]);
